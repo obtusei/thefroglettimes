46bce734af52de0196577a48fe99b18b04d9f5bd
obtusei <54235268+obtusei@users.noreply.github.com>
2022-12-14 06:28:55 +0545

    new changes

--- next.config.js
+++ next.config.js
@@ -1,11 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextTranslate = require('next-translate')
-const nextConfig = nextTranslate({
+// const withPwa = require("next-pwa");
+
+const config = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
     domains: ['s.abcnews.com','imagez.tmz.com','a4.espncdn.com','en.wikipedia.org','upload.wikimedia.org'],
   },
-})
+ pwa: {
+   dest: 'public',
+   register: true,
+   skipWaiting: true,
+    // disable: process.env.NODE_ENV === 'development'
+  }
+}
+// const pwaConfig = withPwa(config)
+
+const nextConfig = nextTranslate(config)
 
 module.exports = nextConfig
